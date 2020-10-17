185f3d70968d586bfa3bbe0aba0a597868ae45e1
mayhem-roberts <reaganmoberts@gmail.com>
2020-10-16 23:03:47 -0600

    commit

--- js/program5-1.js
+++ js/program5-1.js
@@ -1,7 +1,7 @@
 function poundsToKilograms() {
     //input
     let pounds = parseFloat(document.getElementById("pounds").value);
-   //processing
+   //processing: multiply input number of pounds by kilograms
    let output = pounds*0.45359237;
    //output
    document.getElementById("output").innerHTML = output.toFixed(2) + " kilograms ";
