e5cbf5065c4efa8a5814fff1e718ac061944d56c
mayhem-roberts <reaganmoberts@gmail.com>
2020-10-22 21:13:49 -0600

    commit

---
+++ js/footwear.js
@@ -0,0 +1,17 @@
+function getShoes() {
+    let shoes;
+  //input
+  let weather = document.getElementById("weather").value;
+  //processing
+  if (weather === "hot") {
+      shoes = "sandals";
+  } else if (weather === "rain") {
+      shoes = "galoshes";
+  } else if (weather === "snow") {
+      shoes = "oots";
+  } else if (weather === "unknown") {
+      shoes = "shoes";
+  }
+  //output
+  document.getElementById("shoes").innerHTML = " Wear your " + shoes + "!";
+}
